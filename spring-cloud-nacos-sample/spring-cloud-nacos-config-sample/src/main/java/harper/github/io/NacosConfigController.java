77cd084b69c1a3b9280ffd7ed6ad0ff640391e27
yangzhao <15339190665@163.com>
2020-07-11 21:55:42 +0800

    feat --> spring-cloud-nacos 集成

---
+++ spring-cloud-nacos-sample/spring-cloud-nacos-config-sample/src/main/java/harper/github/io/NacosConfigController.java
@@ -0,0 +1,7 @@
+package harper.github.io;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class NacosConfigController {
+}
--- spring-cloud-nacos-sample/spring-cloud-nacos-sample-provider/src/main/java/harper/github/io/user/UserServiceImpl.java
+++ spring-cloud-nacos-sample/spring-cloud-nacos-sample-provider/src/main/java/harper/github/io/user/UserServiceImpl.java
@@ -9,4 +9,9 @@ public class UserServiceImpl implements IUserService{
 
         return "wowowowo";
     }
+
+    public static void main(String args[ ]){
+        int x=4;
+        System.out.println("value is "+ ((x>4) ? 99 :9));
+    }
 }
